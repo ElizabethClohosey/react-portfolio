5e317dd659344dd2138a134a5ccb235c4f211db7
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2021-02-02 12:35:15 -0500

    added icons and color

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import { Zoom, Fade } from "react-reveal";
+import { AiFillHtml5 } from "react-icons/ai";
+import { IoLogoCss3 } from "react-icons/io";
+import { SiJavascript, SiInkscape } from "react-icons/si";
+import { BsBootstrap } from "react-icons/bs";
+import { FaNodeJs, FaReact, FaNpm } from "react-icons/fa";
 
 const Content = () => {
   // const [showGreeting, setShowGreeting] = useState(false);
@@ -10,7 +15,7 @@ const Content = () => {
       <Container className="d-flex flex-column">
         {/* My Projects Section - Project 1 */}
         <Zoom>
-          <Card style={{ width: "18rem", marginBottom: "5rem" }}>
+          <Card style={{ maxWidth: "18rem", marginBottom: "5rem" }}>
             <Card.Img
               variant="top"
               src="#"
@@ -32,7 +37,11 @@ const Content = () => {
         {/* My Projects Section - Project 2 */}
         <Zoom>
           <Card
-            style={{ width: "18rem", marginLeft: "auto", marginBottom: "5rem" }}
+            style={{
+              maxWidth: "18rem",
+              marginLeft: "auto",
+              marginBottom: "5rem",
+            }}
           >
             <Card.Img
               variant="top"
@@ -50,7 +59,7 @@ const Content = () => {
                 <p>Short project description</p>
                 <p>
                   Some quick example text to build on the project title and make
-                  up the bulk of the card's content. 
+                  up the bulk of the card's content.
                 </p>
               </Card.Text>
               {/* <Button variant="primary">Go somewhere</Button> */}
@@ -60,7 +69,7 @@ const Content = () => {
 
         {/* My Projects Section - Project 3 */}
         <Fade left>
-          <Card style={{ width: "18rem", marginBottom: "5rem" }}>
+          <Card style={{ maxWidth: "18rem", marginBottom: "5rem" }}>
             <Card.Img
               variant="top"
               src="#"
@@ -73,9 +82,7 @@ const Content = () => {
             />
             <Card.Body>
               <Card.Title>Project Three</Card.Title>
-              <Card.Text>
-              Short project description
-              </Card.Text>
+              <Card.Text>Short project description</Card.Text>
               {/* <Button variant="primary">Go somewhere</Button> */}
             </Card.Body>
           </Card>
@@ -84,7 +91,11 @@ const Content = () => {
         {/* My Projects Section - Project 4 */}
         <Fade right>
           <Card
-            style={{ width: "18rem", marginLeft: "auto", marginBottom: "5rem" }}
+            style={{
+              maxWidth: "18rem",
+              marginLeft: "auto",
+              marginBottom: "5rem",
+            }}
           >
             <Card.Img
               variant="top"
@@ -98,9 +109,7 @@ const Content = () => {
             />
             <Card.Body>
               <Card.Title>Project Four</Card.Title>
-              <Card.Text>
-              Short project description
-              </Card.Text>
+              <Card.Text>Short project description</Card.Text>
               {/* <Button variant="primary">Go somewhere</Button> */}
             </Card.Body>
           </Card>
@@ -110,22 +119,40 @@ const Content = () => {
       <Container>
         <Zoom>
           <Card style={{ marginBottom: "5rem" }}>
-            <Row>
-              <Col>Icon</Col>
-              <Col>Icon</Col>
-              <Col>Icon</Col>
-              <Col>Icon</Col>
-            </Row>
             <Card.Body>
+              <Row>
+                <Col>
+                  <AiFillHtml5 style={{ fontSize: "2rem", color: "red" }} />
+                </Col>
+                <Col>
+                  <IoLogoCss3 style={{ fontSize: "2rem", color: "#264de4" }} />
+                </Col>
+                <Col>
+                  <SiJavascript style={{ fontSize: "2rem", color: "#f0db4f" }} />
+                </Col>
+                <Col>
+                  <FaNodeJs style={{ fontSize: "2rem", color: "#68a063" }} />
+                </Col>
+              </Row>
+
               <Card.Title>Experience</Card.Title>
               <Card.Text>
-                Add some text about your years of experience and coding bootcamp......?  Maybe..... IDK
+                Add some text about your years of experience and coding
+                bootcamp......? Maybe..... IDK
               </Card.Text>
               <Row>
-                <Col>Icon</Col>
-                <Col>Icon</Col>
-                <Col>Icon</Col>
-                <Col>Icon</Col>
+                <Col>
+                  <FaReact style={{ fontSize: "2rem", color: "#61DBFB" }} />
+                </Col>
+                <Col>
+                  <SiInkscape style={{ fontSize: "2rem" }} />
+                </Col>
+                <Col>
+                  <BsBootstrap style={{ fontSize: "2rem", color: '#563d7c' }} />
+                </Col>
+                <Col>
+                  <FaNpm style={{ fontSize: "2rem", color: '#CC3534' }} />
+                </Col>
               </Row>
               {/* <Button variant="primary">Go somewhere</Button> */}
             </Card.Body>
@@ -149,7 +176,8 @@ const Content = () => {
             <Card.Body>
               <Card.Title>About Me</Card.Title>
               <Card.Text>
-                Talk about your journey in life and career so far.  Add cute picture 
+                Talk about your journey in life and career so far. Add cute
+                picture
               </Card.Text>
               {/* <Button variant="primary">Go somewhere</Button> */}
             </Card.Body>
