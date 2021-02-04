bfe50c88c7b9a852f74b927c8ce562c62c09cb70
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2021-02-04 11:39:00 -0500

    added turquoise to cloud svg. Styled header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,37 +10,30 @@ const Header = () => {
   return (
     <header>
       <Container>
-        <Row
-          className="d-flex justify-content-between"
-        >
-          <Col
-            md={9}
-            className="text-center text-md-left order-0"
-          >
+        <Row className="d-flex justify-content-between">
+          <Col md={9} className="text-center text-md-left order-0">
             <h1>Elizabeth Clohosey</h1>
           </Col>
           <Col
             md={3}
             className="d-flex justify-content-center align-items-center order-3 order-md-2"
           >
-          <a href="https://github.com/ElizabethClohosey">
-            <AiFillGithub style={{ color: "#fafafa", fontSize: '2rem', marginRight: '.5rem'}} />
-          </a>
-          <a href="https://github.com/ElizabethClohosey">
-            <FaLinkedin style={{ color: "#00A0dc", fontSize: '2rem', marginRight: '.5rem'}} />
-          </a>
-          <a href="https://github.com/ElizabethClohosey">
-            <FaFacebook style={{ color: "#3b5998", fontSize: '2rem' }} />
-          </a>
-            {/* <ion-icon name="logo-github" style={{color: 'red'}}></ion-icon>
-              <ion-icon name="logo-linkedin" style={{color: 'green'}}></ion-icon>
-              <ion-icon name="logo-facebook" style={{color: 'blue'}}></ion-icon> */}
+            <a href="https://github.com/ElizabethClohosey">
+              <AiFillGithub className="header-icon" />
+            </a>
+            <a href="https://github.com/ElizabethClohosey">
+              <FaLinkedin className="header-icon" />
+            </a>
+            <a href="https://github.com/ElizabethClohosey">
+              <FaFacebook className="header-icon" />
+            </a>
           </Col>
           <Col
             lg={12}
             className="text-center text-md-left order-2"
+            style={{ color: "white", marginTop: "-15px", fontWeight: "800" }}
           >
-            <p style={{color: 'white', marginTop: '-15px', fontWeight: '800'}}>Fullstack Software Engineer</p>
+            <p>Fullstack Software Engineer</p>
           </Col>
         </Row>
       </Container>
