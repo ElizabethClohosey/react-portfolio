15e5bca97873ae3d3602d4ec6834d0193fe45d69
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2021-02-09 09:34:28 -0500

    added link prefix to match gh pages link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.scss";
 // import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,18 +7,18 @@ import Content from "./components/content";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         {/* <section class="stars-background"> */}
         <div class="stars"></div>
         <div class="twinkling"></div>
         <div class="clouds"></div>
         <div className="wrapper">
-          <Header />
-          <Route exact path="/" render={(props) => <Content {...props} />} />
+          <Header/>
+          <Route exact path="/react-portfolio" render={(props) => <Content {...props} />} />
         </div>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
